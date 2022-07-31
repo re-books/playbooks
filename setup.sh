5a7aa68621fae1d76fea59f57450a6c9fe209495
William <work@suppo.fr>
2022-07-31 17:52:45 +0200

    feat: Rôle d'installation de NodeJS

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 PLAYBOOKS_WD=/srv/playbooks
 PHP_VERSION=8.1
+NODE_VERSION=18.7
 WWW_USER=pilot
 MYSQL_ROOT_PASSWORD="$(date +%s | sha256sum | base64 | head -c 32 ; echo)"
 
@@ -15,5 +16,6 @@ sudo cp hosts.example.ini hosts.ini
 
 ansible-playbook -i hosts.ini playbook.yml --tags "setup" \
   -e php_version=$PHP_VERSION \
+  -e node_version=$NODE_VERSION \
   -e www_user=$WWW_USER \
   -e mysql_root_password="$MYSQL_ROOT_PASSWORD"
